4f9e92cc5215e06b9c98dccf2c5d27aa17118734
Andrew <andrew.dragoslavic@gmail.com>
2023-09-23 23:00:54 -0400

    hello

--- pages/signin.jsx
+++ pages/signin.jsx
@@ -1,51 +1,72 @@
-import { MetaMaskConnector } from "wagmi/connectors/metaMask";
-import { signIn } from "next-auth/react";
-import { useAccount, useConnect, useSignMessage, useDisconnect } from "wagmi";
-import { useRouter } from "next/router";
-import { useAuthRequestChallengeEvm } from "@moralisweb3/next";
+import React, { useState } from 'react';
+import { ethers } from 'ethers';
+
 
 function SignIn() {
-  const { connectAsync } = useConnect();
-  const { disconnectAsync } = useDisconnect();
-  const { isConnected } = useAccount();
-  const { signMessageAsync } = useSignMessage();
-  const { requestChallengeAsync } = useAuthRequestChallengeEvm();
-  const { push } = useRouter();
+    const [walletAddress, setWalletAddress] = useState("");
+    async function requestAccount() {
+        console.log('Requesting account...');
 
-  const handleAuth = async () => {
-    if (isConnected) {
-      await disconnectAsync();
+        // ❌ Check if Meta Mask Extension exists 
+        if(window.ethereum) {
+            console.log('detected');
+
+            try {
+                const accounts = await window.ethereum.request({method: "eth_requestAccounts",});
+                setWalletAddress(accounts[0]);
+            }catch (error) {
+                console.log('Error connecting...');
+            }
+        } else {
+            alert('Meta Mask not detected');
+        }
     }
 
-    const { account, chain } = await connectAsync({
-      connector: new MetaMaskConnector(),
-    });
+    function makeUppercase(address, accounts) {
+        let result = '';
+      
+        for (let i = 0; i < address.length; i++) {
+          if (address[i].toUpperCase() === address[i]) {
+            // console.log(address[i])
+            result += accounts[i].toUpperCase();
+          } else {
+            result += accounts[i];
+          }
+        }
+      
+        return result;
+      }
+    
 
-    const { message } = await requestChallengeAsync({
-      address: account,
-      chainId: chain.id,
-    });
+  // Create a provider to interact with a smart contract
+  async function connectWallet() {
+    if(typeof window.ethereum !== 'undefined') {
+      await requestAccount();
 
-    const signature = await signMessageAsync({ message });
-
-    // redirect user after success authentication to '/user' page
-    const { url } = await signIn("moralis-auth", {
-      message,
-      signature,
-      redirect: false,
-      callbackUrl: "/user",
-    });
-    /**
-     * instead of using signIn(..., redirect: "/user")
-     * we get the url from callback and push it to the router to avoid page refreshing
-     */
-    push(url);
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+    }
   };
-
+  const signData = async() => {
+    const accounts = await window.ethereum.request({method: "eth_requestAccounts",});
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const signer = provider.getSigner()
+    let message = "test message"
+    let signature = await signer.signMessage(message)
+    let address = ethers.utils.verifyMessage(message,signature)
+    console.log(address)
+    console.log(accounts[0])
+    let s = makeUppercase(address, accounts[0])
+    console.log(s)
+    if (address === s) {
+        console.log("SUCCESS!");
+        window.location.href = "http://localhost:3000/user";
+    }
+  }
   return (
     <div>
       <h3>Web3 Authentication</h3>
-      <button onClick={handleAuth}>Authenticate via Metamask</button>
+      <button onClick={requestAccount}>Connect Metamask</button>
+      <button onClick={signData}>Sign in</button>
     </div>
   );
 }
--- pages/user.jsx
+++ pages/user.jsx
@@ -1,32 +1,92 @@
-import { getSession, signOut } from "next-auth/react";
+// https://docs.metamask.io/guide/ethereum-provider.html#using-the-provider
 
-// gets a prop from getServerSideProps
-function User({ user }) {
-  return (
-    <div>
-      <h4>User session:</h4>
-      <pre>{JSON.stringify(user, null, 2)}</pre>
-      <button onClick={() => signOut({ redirect: "/signin" })}>Sign out</button>
-    </div>
-  );
-}
+import React, {useState} from 'react'
+import {ethers} from 'ethers'
 
-export async function getServerSideProps(context) {
-  const session = await getSession(context);
+const User = () => {
 
-  // redirect if not authenticated
-  if (!session) {
-    return {
-      redirect: {
-        destination: "/signin",
-        permanent: false,
-      },
-    };
-  }
+	// deploy simple storage contract and paste deployed contract address here. This value is local ganache chain
+	let contractAddress = '0xF2245ee151D1614004422713a52DA1C98610bA9c';
 
-  return {
-    props: { user: session.user },
-  };
+	const [errorMessage, setErrorMessage] = useState(null);
+	const [defaultAccount, setDefaultAccount] = useState(null);
+	const [connButtonText, setConnButtonText] = useState('Connect Wallet');
+
+	const [currentContractVal, setCurrentContractVal] = useState(null);
+    const ABI = require("./../abi.json");
+	const [provider, setProvider] = useState(null);
+	const [signer, setSigner] = useState(null);
+	const [contract, setContract] = useState(null);
+
+	const connectWalletHandler = () => {
+		if (window.ethereum && window.ethereum.isMetaMask) {
+
+			window.ethereum.request({ method: 'eth_requestAccounts'})
+			.then(result => {
+				accountChangedHandler(result[0]);
+				setConnButtonText('Wallet Connected');
+			})
+			.catch(error => {
+				setErrorMessage(error.message);
+			
+			});
+
+		} else {
+			console.log('Need to install MetaMask');
+			setErrorMessage('Please install MetaMask browser extension to interact');
+		}
+	}
+
+	// update account, will cause component re-render
+	const accountChangedHandler = (newAccount) => {
+		setDefaultAccount(newAccount);
+		updateEthers();
+	}
+
+	const updateEthers = () => {
+		let tempProvider = new ethers.providers.Web3Provider(window.ethereum);
+		setProvider(tempProvider);
+
+		let tempSigner = tempProvider.getSigner();
+		setSigner(tempSigner);
+
+		let tempContract = new ethers.Contract(contractAddress, ABI, tempSigner);
+		setContract(tempContract);	
+	}
+
+	const setHandler = async (event) => {
+		event.preventDefault();
+		const buyPrice = await contract.getBuyPriceAfterFee('0x1b8FAF40eB34f67c3821525141E88F331e8fc4c0',1);
+        console.log('Name: ' + buyPrice)
+        setSigner(provider.getSigner());
+        await ethereum.request({ method: "eth_requestAccounts" });
+        await contract.buyaccessTokens(buyPrice, '0x1b8FAF40eB34f67c3821525141E88F331e8fc4c0', 1);
+
+	}
+
+	const getCurrentVal = async () => {
+		let val = await contract.get();
+		setCurrentContractVal(val);
+	}
+	
+	return (
+		<div>
+		<h4> {"Get/Set Contract interaction"} </h4>
+			<button onClick={connectWalletHandler}>{connButtonText}</button>
+			<div>
+				<h3>Address: {defaultAccount}</h3>
+			</div>
+			<form onSubmit={setHandler}>
+				<input id="setText" type="text"/>
+				<button type={"submit"}> Update Contract </button>
+			</form>
+			<div>
+			<button onClick={getCurrentVal} style={{marginTop: '5em'}}> Get Current Contract Value </button>
+			</div>
+			{currentContractVal}
+			{errorMessage}
+		</div>
+	);
 }
 
 export default User;
